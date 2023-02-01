56a46fd6c6b42bb0f3128c9089de53ffd038f381
TG Frerich <themuppet.coder@gmail.com>
2023-02-01 14:54:01 +0100

    First steps

---
+++ src/test/java/org/example/GuestListTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuestListTest {
+
+@Test
+public void shouldBeEmptyInitially(){
+    GuestList guestList =new GuestList();
+    List<String>
+}
+
+}
